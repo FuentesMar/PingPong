f63e79593be2b2ed80b11fb50baf0c475e77435e
EidtanFuentes <112510183+EidtanFuentes@users.noreply.github.com>
2022-10-20 20:13:05 -0700

    Practicado

---
+++ src/Ventana.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+
+    Tablero canvas;
+    Pelota p = new Pelota();
+
+    public Ventana() {
+        setTitle("Ping Pong Game");
+        setSize(800, 500);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        canvas = new Tablero();
+        add(canvas);
+
+        addKeyListener(new EventoTeclado());
+        new EngineGraphics(canvas).start();
+    }
+
+}
